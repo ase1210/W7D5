3329ec1c4bf3c2341ec27b8d0dbf775b0127a9d6
DrewE <andrew.engelstein@gmail.com>
2019-02-22 16:04:23 -0800

    Create rails project, backend user auth

---
+++ BenchBnB/app/controllers/application_controller.rb
@@ -0,0 +1,24 @@
+class ApplicationController < ActionController::Base
+  helper_method :logged_in?, :current_user
+  def login!(user)
+    session[:sesson_token] = user.reset_session_token!
+    @current_user = user
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
+  def current_user
+    @current_user ||= User.find_by_session_token(session[:session_token])
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  # def require_login
+  #   redirect_to api_new_session_url unless current_user
+  # end
+end
---
+++ BenchBnB/frontend/benchBnB.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const groot = document.getElementById('groot')
+  ReactDOM.render(<h1>YAYYYYY</h1>, groot)
+});
